f9493cc7c7916b4177ca989762bd33de79c05a58
sizaro <spartansiza@gmail.com>
2025-10-23 15:26:03 +0300

    css

--- src/pages/landing/Home.tsx
+++ src/pages/landing/Home.tsx
@@ -11,7 +11,15 @@ const Home = () => {
             "url('/images/womenempoeringgirlshero.jpg')",
         }}
       >
-        <div className="relative z-10 text-white px-6 bottom-0">
+        <div className="absolute z-10 text-white px-6 bottom-6 left-0 hidden md:block">
+
+          <h1 className="text-4xl md:text-5xl font-bold mb-4">
+              Empowering Girls for a Brighter Future
+            </h1>
+            <p className="max-w-2xl mx-auto mb-8 text-lg">
+              We inspire confidence, provide education, and nurture life skills to build a generation of strong, resilient women.
+            </p>
+
           <a
             href="/get-involved"
             className="bg-white text-pink-700 font-semibold px-6 py-3 rounded-lg hover:bg-pink-700 hover:text-white transition bottom-0"
@@ -20,6 +28,28 @@ const Home = () => {
           </a>
         </div>
       </section>
+
+      <section
+        className=" bg-gray bg-center flex items-center justify-center text-center md:hidden mb-13"
+      >
+        <div className="relative px-6">
+
+          <h1 className="text-pink-700 text-4xl md:text-5xl font-bold mb-4 text-left">
+              Empowering Women and Girls for a Brighter Future
+            </h1>
+            <p className="text-black-700 max-w-2xl mx-auto text-lg text-left">
+              We inspire confidence, provide education, and nurture life skills to build a generation of strong, resilient women.
+            </p>
+
+          <a
+            href="/get-involved"
+            className="absolute bottom--4 left-0 bg-red-300 text-pink-700 font-semibold px-6 py-3 rounded-lg hover:bg-pink-700 hover:text-white transition left-4"
+          >
+            Get Involved
+          </a>
+        </div>
+      </section>
+
 
       {/* Mission Section */}
       <section className="py-20 bg-pink-50 text-center">
