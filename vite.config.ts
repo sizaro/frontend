cfaaac985f796ff303fa260c48f4def5716dedf3
sizaro <spartansiza@gmail.com>
2025-10-23 16:08:48 +0300

    allowed

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: ['womenempoweringgirls.onrender.com', 'localhost']
+  }
 })
